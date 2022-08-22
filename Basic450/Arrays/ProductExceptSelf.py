00d756a55001f4778966bd2c129b2f3095f48028
SaiVenkatDhulipudi <82017014+SaiVenkatDhulipudi@users.noreply.github.com>
2022-08-22 11:49:34 +0530

    Create ProductExceptSelf.py

---
+++ Basic450/Arrays/ProductExceptSelf.py
@@ -0,0 +1,20 @@
+def productExceptSelf(a, n):
+  n=len(a)
+  prod = 1
+  flag = 0
+  for i in range(n):
+      if (a[i] == 0):
+          flag += 1
+      else:
+          prod *= a[i]
+  arr = [0 for i in range(n)]
+  for i in range(n):
+      if (flag > 1):
+          arr[i] = 0
+      elif (flag == 0):
+          arr[i] = (prod // a[i])
+      elif (flag == 1 and a[i] != 0):
+          arr[i] = 0
+      else:
+          arr[i] = prod
+  return arr
